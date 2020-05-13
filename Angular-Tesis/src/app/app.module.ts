85b7a86ccf1a2574fa552e051b436e0854b85d8c
lebafu <50503857+lebafu@users.noreply.github.com>
2020-05-13 20:31:09 -0300

    Formulario Regisro Alumno

--- Angular-Tesis/src/app/app.module.ts
+++ Angular-Tesis/src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {Route, RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { AlumnoFormComponent } from './alumno-form/alumno-form.component';
+import { HomeComponent } from './home/home.component';
+const routes: Route[]=[
+{path:'', component: HomeComponent},
+{path:'users',component:UsersComponent},
+{path:'alumno-form', component: AlumnoFormComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    AlumnoFormComponent
+    AlumnoFormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
