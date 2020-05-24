f6a1a3ecdf27e09969c9d399d8bee5c043317728
lebafu <50503857+lebafu@users.noreply.github.com>
2020-05-24 02:26:03 -0300

    login en parte de angular funcionando, falta corregir error en laravel
    login en parte de angular funcionando, falta corregir error en laravel

--- Angular-Tesis/src/app/components/login/login.component.ts
+++ Angular-Tesis/src/app/components/login/login.component.ts
@@ -17,31 +17,30 @@ export class LoginComponent implements OnInit {
     password: null
   };
 
-  public error = null;
+  public error = [];
 
   constructor(
     private Jarwis: JarwisService,
     private Token: TokenService,
-    private router: Router,
-    private Auth: AuthService
+    private router: Router
   ) { }
 
   onSubmit() {
+    //console.log(this.form);
     this.Jarwis.login(this.form).subscribe(
       data => this.handleResponse(data),
       error => this.handleError(error)
     );
   }
-
   handleResponse(data) {
     this.Token.handle(data.access_token);
-    this.Auth.changeAuthStatus(true);
     this.router.navigateByUrl('/profile');
   }
 
   handleError(error) {
     this.error = error.error.errors;
   }
+
   ngOnInit() {
   }
 
