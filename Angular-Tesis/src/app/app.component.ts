7f3659c276f502061bb4724d2df1b624cc1e737f
lebafu <50503857+lebafu@users.noreply.github.com>
2020-05-27 03:22:34 -0300

    Logout y Login funcionando falta que barra se carguee automaticamente al iniciar sesión
    Logout y Login funcionando falta que barra se carguee automaticamente al iniciar sesión

--- Angular-Tesis/src/app/app.component.ts
+++ Angular-Tesis/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Tesis';
+  title = 'Angular-Tesis';   //AQUI ESTA LA CLAVE
+ public loggedIn: boolean;
+
+  constructor(
+    private Auth: AuthService,
+    private router: Router,
+    private Token: TokenService
+  ) {}
+  
+   ngOnInit() {
+   this.Auth.authStatus.subscribe(value => this.loggedIn = value);
+    console.log(this.loggedIn);
+   }
+logout(event: MouseEvent) {
+    event.preventDefault();
+    this.Token.remove();
+    this.Auth.changeAuthStatus(false);
+    this.router.navigateByUrl('/login');
+  }
 }
